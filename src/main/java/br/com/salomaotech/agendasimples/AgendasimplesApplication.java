c9994bb518201a02508f3bb58ae89474c62e4b31
salomaotech <salomao@taimber.com>
2022-02-11 12:17:55 -0300

    Primeiro envio

---
+++ src/main/java/br/com/salomaotech/agendasimples/AgendasimplesApplication.java
@@ -0,0 +1,13 @@
+package br.com.salomaotech.agendasimples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendasimplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendasimplesApplication.class, args);
+	}
+
+}
